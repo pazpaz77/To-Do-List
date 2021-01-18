740952375a7d2aeecc58c1f73cbbcde08d12eae0
PazitC <pazit.tp@gmail.com>
2021-01-18 10:21:36 +0200

    add basic Todo class

---
+++ src/TodoList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+class TodoList extends React.Component {
+    constructor(props) {
+    super(props);
+
+    this.state = {
+    id:' ',
+    itemName: ' ',
+    completed: false,
+    };
+
+    }
+
+    render() {
+        
+        return (
+           
+          <div></div>
+       
+            
+        )
+       
+    }
+}
+
+export default TodoList;
